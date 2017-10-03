5b02fb2504ef81d895a008e5193b86dbb8a48009
Truong Khuong <khuongxuantruong@gmail.com>
2017-10-03 17:12:00 +0700

    cds

---
+++ server/assets/js/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    $('#fullpage').fullpage({
+        sectionsColor: ['#f6f6f6', '#12171e', '#12171e', 'whitesmoke', '#ccddff'],
+        anchors: ['home', 'about', 'our-brand', '4thpage', 'lastPage'],
+        menu: '#menu',
+        css3: true,
+        scrollingSpeed: 1000
+    });
+});
